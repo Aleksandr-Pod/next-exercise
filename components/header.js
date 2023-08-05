d5407bc464cc412c1d123f1be8758b4351baf43d
Aleksandr-Pod <stdfire@gmail.com>
2023-08-05 08:23:26 +0300

    styles

--- components/header.js
+++ components/header.js
@@ -1,4 +1,4 @@
-import { Section } from "./header.styled";
+import { Section, NavButton } from "./header.styled";
 import Link from "next/link";
 
 const Header = () => (
@@ -6,9 +6,9 @@ const Header = () => (
         <Link href='/'><h2>Logo</h2></Link>
         <nav>
             <ul>
-                <Link href='infoPage'>Info page</Link>
-                <Link href='details1'>Details - 1</Link>
-                <Link href='details2'>Details - 2</Link>
+                <Link href='infoPage'><NavButton>Info page</NavButton></Link>
+                <Link href='details1'><NavButton>Details - 1</NavButton></Link>
+                <Link href='details2'><NavButton>Details - 2</NavButton></Link>
             </ul>
         </nav>
     </Section>
--- components/header.styled.js
+++ components/header.styled.js
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 export const Section = styled.header`
+    padding: 0 10px;
     display: flex;
     justify-content: space-between;  
     height: 60px;
     background-color: purple;
     color: yellow;
+    align-items: center;
+`
+export const NavButton = styled.a`
+    padding: 5px 10px;
+    margin-left: 10px; 
     text-align: center;
+    font-size: 16px;
+    color: yellow;
+    background-color: gray;
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover {background-color: darkgray;}
 `
